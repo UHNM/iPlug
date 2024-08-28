772a1c3d8c362bf9919878be6c8a5c1f3630d3d8
chris maddock <chris.maddock@uhnm.nhs.uk>
2024-08-28 11:47:32 +0100

    config for api url

--- iPlug/Components/Pages/DynamicForm2.razor.cs
+++ iPlug/Components/Pages/DynamicForm2.razor.cs
@@ -1,26 +1,23 @@
 using Microsoft.AspNetCore.Components;
-using JJMasterData.Core.DataDictionary.Models;
-using JJMasterData.Core.UI.Components;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Configuration;
 
 namespace iPlug.Components.Pages
 {
     public partial class DynamicForm2
     {
+        [Inject]
+        private IConfiguration configuration { get; set; }
+
+
         [ViewData]
         public  string PanelViewHtml { get; set; }
-        /// <summary>
-        /// [Inject]
-        /// </summary>
-        // private HttpClient Http { get; set; }
-
-
+      
         protected override async Task OnInitializedAsync()
         {
             HttpClient client = new HttpClient();
-
-            var response  = await client.GetAsync("http://localhost:5260/dynamicform");
+            var url = configuration["DynamicFormsApiUrl"];
+            var response  = await client.GetAsync(url);
             PanelViewHtml = await response.Content.ReadAsStringAsync();
           
 
