2146459495000a6cf585e07553126c74ecf29129
chris maddock <chris.maddock@uhnm.nhs.uk>
2024-08-28 15:51:05 +0100

    save method

--- DynamicFormApi/Controllers/DynamicFormController.cs
+++ DynamicFormApi/Controllers/DynamicFormController.cs
@@ -2,6 +2,7 @@
 using JJMasterData.Core.DataDictionary.Models;
 using JJMasterData.Core.UI.Components;
 using JJMasterData.Commons.Data.Entity.Repository.Abstractions;
+using JJMasterData.Commons.Data.Entity.Repository;
 
 namespace DynamicFormApi.Controllers
 {
@@ -10,22 +11,24 @@ namespace DynamicFormApi.Controllers
     public class DynamicFormController : Controller
     {
 
-       
+
+        private readonly IFormElementComponentFactory<JJFormView> _formViewFactory;
         private readonly IComponentFactory _componentFactory;
-     
+        private readonly IEntityRepository _entityRepository;
+
 
 
         public required string PanelViewHtml { get; set; }
 
 
-        public DynamicFormController(IComponentFactory componentFactory)
+        public DynamicFormController(ILogger<DynamicFormController> logger, IFormElementComponentFactory<JJFormView> formViewFactory, IComponentFactory componentFactory, IEntityRepository entityRepository)
         {
             _componentFactory = componentFactory;
 
         }
-
-        [HttpGet(Name = "GetDynamicForm")]
-        public async Task<string> Get()
+        [Route("getdynamicform"), HttpGet]
+      //  [HttpGet(Name = "GetDynamicForm")]
+        public async Task<string> GetDynamicForm()
         {
             var dataPanel = await GetDataPanel();
 
@@ -40,11 +43,52 @@ namespace DynamicFormApi.Controllers
             return PanelViewHtml;
         }
 
+
+
+        [Route("save"), HttpPost]
+        public async Task<IActionResult> Save()
+        {
+            var dataPanel = await GetDataPanel();
+            var values = await dataPanel.GetFormValuesAsync();
+
+            return Ok(values);
+        }
+
         private async Task<JJDataPanel> GetDataPanel()
         {
             var dataPanel = await _componentFactory.DataPanel.CreateAsync("Ventilation");
             dataPanel.PageState = PageState.Update; // You can change here to PageState.Insert if you want.
             return dataPanel;
+        }
+
+      
+
+        [HttpPost]
+        public async Task<IActionResult> Save2()
+        {
+            var dataPanel = await GetDataPanel();
+            var values = await dataPanel.GetFormValuesAsync();
+            var errors = dataPanel.ValidateFields(values);
+
+            if (errors.Count > 0)
+            {
+                dataPanel.Errors = errors;
+                ViewData["ReturnMessage"] = _componentFactory.Html.ValidationSummary.Create(errors).GetHtml();
+            }
+            else
+            {
+                //Update values in Database
+                await _entityRepository.InsertAsync(dataPanel.FormElement, values);
+
+                var alert = _componentFactory.Html.Alert.Create();
+                //alert.Color = PanelColor.Success;
+                alert.Title = "Record updated";
+                ViewData["ReturnMessage"] = alert.GetHtml();
+            }
+            var result = await dataPanel.GetResultAsync();
+            PanelViewHtml = result.Content;
+
+            return View("Index");
         }
     }
 }
--- iPlug/Components/Pages/DynamicForm2.razor.cs
+++ iPlug/Components/Pages/DynamicForm2.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
@@ -9,17 +10,39 @@ namespace iPlug.Components.Pages
         [Inject]
         private IConfiguration configuration { get; set; }
 
+        public static int InitialCount = 0;
 
         [ViewData]
         public  string PanelViewHtml { get; set; }
       
         protected override async Task OnInitializedAsync()
         {
+
+            //if (InitialCount < 2)
+            //{
+               // InitialCount++;
+                HttpClient client = new HttpClient();
+                var url = configuration["DynamicFormsApiUrl"] + "/getdynamicform";
+                var response = await client.GetAsync(url);
+                PanelViewHtml = await response.Content.ReadAsStringAsync();
+                //IsIntial = true;
+
+            //}
+
+
+        }
+
+
+        public async Task OnButtonClick(MouseEventArgs e)
+        {
+           
             HttpClient client = new HttpClient();
-            var url = configuration["DynamicFormsApiUrl"];
-            var response  = await client.GetAsync(url);
-            PanelViewHtml = await response.Content.ReadAsStringAsync();
-          
+
+            var url = configuration["DynamicFormsApiUrl"] + "/save";
+            var response = await client.PostAsync(url, null);
+
+           
+
 
         }
     }
